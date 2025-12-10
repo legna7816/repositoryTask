355dac2fea09d0f32da78400df84a605804d2903
legna7816 <tkdqlsfl02@gmail.com>
2025-12-11 02:27:49 +0900

    master changed it

--- GitTask/src/myGit/C.java
+++ GitTask/src/myGit/C.java
@@ -4,6 +4,6 @@ public class C {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
         int sum = arr[0] + arr[1] + arr[2];
-        System.out.println("Sum: " + sum);
+        System.out.println("Total Sum: " + sum);
     }
 }
